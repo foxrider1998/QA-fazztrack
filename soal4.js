7a390d10273a337803b94b05d9834816fc2f08dc
Aris <areskretindo@gmail.com>
2022-11-20 09:09:14 +0700

    update soal4

--- soal4.js
+++ soal4.js
@@ -1,3 +1,17 @@
-const fetch = require('node-fetch');
-const data =fetch('https://jsonplaceholder.typicode.com/users')
-console.log(data)
+
+import fetch from 'node-fetch';
+
+
+
+const nama =async   ()=>{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+    data.forEach(element => {
+        console.log(element.name)
+    });
+    
+
+}
+
+nama()
